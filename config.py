64bf7e8b582f6c0b414da75c1287c98a955e51d3
iyan437 <ianmunene1417@gmail.com>
2026-07-27 18:41:33 +0300

    Update configuration with new API token and email
    Updated sensitive configuration values for API token and email credentials.

---
+++ config.py
@@ -0,0 +1,6 @@
+DERIV_TOKEN = 'pat_72bcf0725c330e71ffd25b546e86132340b34272ed649225710a4e63bb458f71'
+EMAIL_USER = 'ianmunene1417@gmail.com'
+EMAIL_PASS = 'umee qpcb odcd qssy'
+TO_EMAIL = 'ianmunene1417@gmail.com'
+
+SYMBOL = 'R_100'
